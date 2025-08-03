a892857941f22973489ba1cf4e81fb80f2a3dd67
Ryze920 <123293103+Ryze920@users.noreply.github.com>
2025-08-03 15:55:43 +0700

    Fitur: Tambahkan gambar profil baru dan perbarui deskripsi tentang diri - Ganti gambar profil dengan 'yosua2.jpg' dan perbarui teks di bagian 'About Me' serta 'Dev Skills'.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -7,18 +7,18 @@ const AboutMe = (about) => {
         <div className="flex flex-col xl:flex-row  m-8 items-center justify-between xl:pt-8 xl:pb-24">
           {/* TEXT */}
           <div className="text-center xl:text-left order-2 xl:order-none">
-            <span className="text-xl ps-5">Yosua Kurniawan</span>
             <h1 className="h1 mb-6">
               About Me <br />{" "}
               <span className="text-transparent bg-clip-text bg-gradient-to-b from-purple-900 to-violet-500">
                 The Random Guy
               </span>
             </h1>
-            <p className="max-w-[500px]">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Praesentium sapiente fugit consequuntur? Minima, molestiae.
-              Ratione vero illo molestias consequatur quis, asperiores quia in
-              at quisquam nostrum. Alias, aperiam. Veniam, itaque.
+            <p className="max-w-[700px] text-2xl">
+              I have a passion for the digital world. As a Frontend Developer, I
+              love turning ideas into interactive interfaces. My hobby in
+              photography and experience as a Social Media Designer complements
+              my skills with a creative touch. Oh yeah, greetings from my cat,
+              Thanos.
             </p>
           </div>
 
--- src/components/Devskils/Textskills.jsx
+++ src/components/Devskils/Textskills.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const Textskills = () => {
   return (
-    <div className="flex flex-col items-center mt-[100px]">
+    <div className="flex flex-col items-center mt-[100px] ">
       <h2 className="text-6xl bg-gradient-to-b from-violet-900 to-white text-transparent bg-clip-text mb-10 font-bold">
         Dev Skills
       </h2>
-      <p className="text-lg text-center text-white">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum porro
-        facilis incidunt dolorum sed repellendus, quas libero commodi, inventore
-        ad quod illum molestias fugit blanditiis architecto excepturi natus,
-        esse consequatur quam! Cum doloremque porro, soluta nobis voluptate quod
-        molestias dignissimos numquam, quae, ipsa impedit ex laboriosam?
-        Eligendi quo voluptates suscipit!
+      <p className="text-lg text-center text-white max-w-[1000px]">
+        I use technologies for web and app development, including basic
+        languages such as HTML, CSS, and JavaScript. I have experience with
+        popular frameworks and libraries, namely React and Tailwind CSS, and I
+        am familiar with Bootstrap to speed up the design process. On the
+        backend side, I am proficient in using Node.js and PHP. In addition, I
+        also have video editing skills with DaVinci Resolve.
       </p>
     </div>
   );
